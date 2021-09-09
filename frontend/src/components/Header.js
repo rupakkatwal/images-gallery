39dca741f782f9a6150acbc536101aa6e2a43393
rupakkatwal <rupak2072@gmail.com>
2021-09-09 12:37:38 +0200

    intial header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Navbar } from 'react-bootstrap';
 
-const Header = () => {
+const Header = ({title}) => {
     return(
         <Navbar bg="dark" variant="Light">
-             <Navbar.Brand href="/">Images Gallery</Navbar.Brand>
+             <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Navbar>
     )
 };
