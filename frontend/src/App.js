8d1bed3dc55992807fdcc2c67efba3a73deb88c5
rupakkatwal <rupak2072@gmail.com>
2021-09-12 04:10:36 +0200

    render and display search images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,44 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
 import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
-
-const UNSPLASH_KEY  = process.env.REACT_APP_UNSPLASH_KEY
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
 
 function App() {
-  
+
   const [word, setWord] = useState('')
-  const [images,setImages] = useState([]);
-  const handleSearchSubmit = (e) =>{
+  const [images, setImages] = useState([]);
+  const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      setImages([data, ...images])
-      console.log(images)
-    })
-    .catch((err) =>{
-      console.log(err)
-    })
-  setWord('');
+      .then((res) => res.json())
+      .then((data) => {
+        setImages([{ ...data, title: word }, ...images]);
+        console.log(images);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    setWord('');
   }
   return (
     <div className="App">
-     <Header title = "Images Gallery"/>
-     <Search word  = {word} setWord = {setWord} handleSubmit = {handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          {images.map((image, i) => (
+            <Col key={i}>
+              <ImageCard image={image} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+
     </div>
   );
 }
